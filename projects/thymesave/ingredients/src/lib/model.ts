7bb2e777bb1c695cd060a0d360c41a9d4b31e4fd
Timo Reymann <mail@timo-reymann.de>
2022-01-22 19:30:24 +0100

    feat(#59): Add initial ingredient parsing

--- projects/thymesave/ingredients/src/lib/model.ts
+++ projects/thymesave/ingredients/src/lib/model.ts
@@ -43,3 +43,12 @@ export interface RecipeIngredient {
    */
   ingredient: string
 }
+
+export interface RecipeIngredient {
+  unit : string | null
+  minAmount ?: string|number
+  maxAmount ?: string|number
+  isNumeric : boolean
+  isRange : boolean
+  ingredient ?: string
+}
