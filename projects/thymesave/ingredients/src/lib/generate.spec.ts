40b468a71e04049d49413b0f9335cd7a7f195982
Timo Reymann <mail@timo-reymann.de>
2022-02-11 22:49:50 +0100

    chore: Remove unused helper test to generate initial translation structure

--- projects/thymesave/ingredients/src/lib/generate.spec.ts
+++
@@ -1,9 +0,0 @@
-import ingredients from "./ingredients";
-
-describe("blub", () => {
-  it("ingredients", () => {
-    for (let key in ingredients) {
-     console.log(`| "${key}": ["${key.split("_").join(" ")}"],`)
-    }
-  })
-})
