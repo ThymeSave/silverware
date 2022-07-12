a7f2e59154b64f5e4da5d318ea0f5132a673fdff
Timo Reymann <mail@timo-reymann.de>
2022-07-12 21:23:38 +0200

    chore: Cleanup i18n pipe

--- src/app/shared/i18n/i18n.pipe.ts
+++ src/app/shared/i18n/i18n.pipe.ts
@@ -1,16 +1,13 @@
 import { ChangeDetectorRef, Injector, OnDestroy, Pipe, PipeTransform } from '@angular/core';
 import { loadUITextByKey } from "@thymesave/translations"
 import { LanguageService } from "./language.service";
-import { AsyncPipe } from "@angular/common";
 import { map, switchMap } from "rxjs";
 
 @Pipe({
   name: 'i18n',
   pure: false,
 })
-export class I18nPipe implements PipeTransform, OnDestroy {
-  private asyncPipe: AsyncPipe;
-
+export class I18nPipe implements PipeTransform {
   constructor(private languageService: LanguageService,
               private injector: Injector) {
     this.asyncPipe = new AsyncPipe(injector.get(ChangeDetectorRef));
@@ -18,9 +15,5 @@ export class I18nPipe implements PipeTransform, OnDestroy {
 
   transform(value: string): string  {
     return loadUITextByKey(this.languageService.currentLanguage, value)
-  }
-
-  ngOnDestroy() {
-    this.asyncPipe.ngOnDestroy();
   }
 }
