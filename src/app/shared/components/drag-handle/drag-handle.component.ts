21395338d059e1c2807db8d3c83636d75f2cd70f
Timo Reymann <mail@timo-reymann.de>
2022-08-13 12:32:09 +0200

    fix: Adjust color palette to match design

--- src/app/shared/components/drag-handle/drag-handle.component.ts
+++ src/app/shared/components/drag-handle/drag-handle.component.ts
@@ -3,14 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-drag-handle',
   styles: [`
-  mat-icon {
-    cursor: pointer;
-    transition: all .1s ease-in-out;
+    @use "../../../../theme";
 
-    &:hover {
-      color: var(--primary-color);
+    mat-icon {
+      cursor: pointer;
+      transition: all .1s ease-in-out;
+
+      &:hover {
+        color: theme.get-color("primary", "main");
+      }
     }
-  }
   `],
   template: `
     <mat-icon>drag_handle</mat-icon>
